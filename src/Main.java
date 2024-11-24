f012f5db4b212686f8f6c6eedd4d05ee08a1d680
Ilya Elichev <git.user.ilyaelichev@gmail.com>
2024-11-24 23:28:35 +0400

    Добавлен файл README

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+        int sum = firstNumber + secondNumber;
+        int difference = firstNumber - secondNumber;
+        int multiply = firstNumber * secondNumber;
+        double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Сумма чисел: " + sum);
+        System.out.println("Разность чисел: " + difference);
+        System.out.println("Произведение чисел: " + multiply);
+        System.out.println("Частное чисел: " + quotient);
     }
 }
