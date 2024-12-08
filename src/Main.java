4f5f2574995c98fcb20370473aceb8a4ffd74731
Ilya Elichev <git.user.ilyaelichev@gmail.com>
2024-12-08 19:06:41 +0400

    В метод main добавлена логика: она будет определять существует ли файл, путь к которому был указан

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println( "Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        Scanner scanner = new Scanner(System.in);
+        int fileCount = 0;
+
+        while (true) {
+            System.out.println("Введите путь и нажмите <Enter>: ");
+            String path = scanner.nextLine();
+            File file = new File(path);
+            boolean fileExist = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExist) {
+                System.out.println("Путь не существует. Попробуйте снова.");
+                continue;
+            }
+
+            if (isDirectory) {
+                System.out.println("Это директория, а не файл. Попробуйте снова.");
+                continue;
+            }
+
+            fileCount++;
+            System.out.println("Путь указан верно. Это файл номер " + fileCount);
+        }
     }
 }
